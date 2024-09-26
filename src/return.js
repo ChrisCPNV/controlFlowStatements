7a109a065d0c64277c4512d7ea2a2c394719bf73
Chris <pl52dsy@eduvaud.ch>
2024-09-26 16:36:59 +0200

    return pas encore finis

--- src/return.js
+++ src/return.js
@@ -1,8 +1,26 @@
 const numbers = [];
+let startNumber = 100;
+
+function stopCodeIf (){
+    for (let i = 0; i < numbers.length; i++) {
+        console.log(numbers[i]);
+
+        if (numbers[i] % 2 === 0) {
+
+            if (i > numbers.length) {
+                return startNumber;
+            }
+            if (startNumber === 50){
+                return startNumber;
+            }
+
+            startNumber -= numbers[i];
+        }
+    }
+}
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-}
+console.log(stopCodeIf());
