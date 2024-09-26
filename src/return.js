6f22b46940b746aa28b66f1bd90f105dc01204b9
Chris <pl52dsy@eduvaud.ch>
2024-09-26 21:25:34 +0200

    feat: return asap

--- src/return.js
+++ src/return.js
@@ -1,26 +1,27 @@
 const numbers = [];
-let startNumber = 100;
 
-function stopCodeIf (){
+function stopCodeIf (startNumber){
     for (let i = 0; i < numbers.length; i++) {
         console.log(numbers[i]);
-
         if (numbers[i] % 2 === 0) {
 
-            if (i > numbers.length) {
-                return startNumber;
+            if (i > numbers.length / 2 ) {
+                return "Plus de la moitié de la liste à été traitée, somme restante : " + startNumber;
             }
             if (startNumber === 50){
-                return startNumber;
+                return "Le nombre est égale à exactement : " + startNumber;
             }
-
+            if (numbers.indexOf(numbers[i]) !==i){
+                return "Un nombre à soustraire était 2 fois dans la liste. La somme : " + startNumber;
+            }
             startNumber -= numbers[i];
         }
     }
+    return "La liste entière a été traitée, somme restante : " + startNumber;
 }
 
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
-console.log(stopCodeIf());
+console.log(stopCodeIf(100));
