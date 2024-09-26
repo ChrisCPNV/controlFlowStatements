7fc6fe22222180caf4029eec45c472d7be8a66dd
Chris <pl52dsy@eduvaud.ch>
2024-09-26 16:09:58 +0200

    feat: continue better

--- src/continue.js
+++ src/continue.js
@@ -1,8 +1,13 @@
 const numbers = [];
+let somme = 0
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+    if (numbers[i] < somme) continue;
+    somme += numbers[i];
 }
+console.log("La somme des nombres : " + somme);
